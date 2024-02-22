9103265534969e28450caa26a2548b74502b5197
mustafaxoodiye <mustafaxoodiye@gmail.com>
2024-02-22 10:56:00 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, MatButtonModule, MatInputModule, MatFormFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'demo-app';
+
+  onPrimengClick(value: string) {
+    console.log('Your Favorite meal is: ', value);
+  }
+
+  onAMClick(value: string) {
+    console.log('Your Favorite meal is: ', value);
+
+  }
 }
